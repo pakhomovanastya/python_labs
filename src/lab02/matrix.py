a882ef520cb2621c4fa07be0231a314cb5743c8b
pakhomovanastya <pakhomova.nastya.lol@gmail.com>
2025-10-06 23:25:33 +0300

    Update matrix.py

--- src/lab02/matrix.py
+++ src/lab02/matrix.py
@@ -6,12 +6,12 @@ def transpose(mat: list[list[float | int]]) -> list[list]:
         if len(a)!=len_mat:
             raise ValueError('строки разной длины')
     return [[a[x] for a in mat] for x in range(len_mat)]
-print('transpose')
-print(transpose(mat=[[1, 2, 3]]))
-print(transpose(mat=[[1], [2], [3]]))
-print(transpose(mat=[[1, 2], [3, 4]]))
-print(transpose(mat=[]))
-print(transpose(mat=[[1, 2], [3]]))
+# print('transpose')
+# print(transpose(mat=[[1, 2, 3]]))
+# print(transpose(mat=[[1], [2], [3]]))
+# print(transpose(mat=[[1, 2], [3, 4]]))
+# print(transpose(mat=[]))
+# print(transpose(mat=[[1, 2], [3]]))
 
 def row_sums(mat: list[list[float | int]]) -> list[float]:
     if len(mat)==0:
@@ -21,11 +21,11 @@ def row_sums(mat: list[list[float | int]]) -> list[float]:
         if len(a)!=len_mat:
             raise ValueError('строки разной длины')
     return [sum(a) for a in mat]
-print('row_sums')
-print(row_sums(mat=[[1, 2, 3], [4, 5, 6]]))
-print(row_sums(mat=[[-1, 1], [10, -10]]))
-print(row_sums(mat=[[0, 0], [0, 0]]))
-print(row_sums(mat=[[1, 2], [3]]))
+# print('row_sums')
+# print(row_sums(mat=[[1, 2, 3], [4, 5, 6]]))
+# print(row_sums(mat=[[-1, 1], [10, -10]]))
+# print(row_sums(mat=[[0, 0], [0, 0]]))
+# print(row_sums(mat=[[1, 2], [3]]))
 
 def col_sums(mat: list[list[float | int]]) -> list[float]:
     if len(mat)==0:
