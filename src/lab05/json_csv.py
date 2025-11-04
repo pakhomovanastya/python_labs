5defedaa80dd10cb12f1467c5fe02a76324ea452
pakhomovanastya <pakhomova.nastya.lol@gmail.com>
2025-11-04 14:47:43 +0300

    1

--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -1,10 +1,13 @@
+import json
 def json_to_csv(json_path: str, csv_path: str) -> None:
     """
     Преобразует JSON-файл в CSV.
     Поддерживает список словарей [{...}, {...}], заполняет отсутствующие поля пустыми строками.
     Кодировка UTF-8. Порядок колонок — как в первом объекте или алфавитный (указать в README).
     """
-    pass #пустая ф.
+    with open(json_path , encoding="utf-8") as f:
+        d = json.load(f)
+        print(d)
 
 def csv_to_json(csv_path: str, json_path: str) -> None:
     """
@@ -13,3 +16,6 @@ def csv_to_json(csv_path: str, json_path: str) -> None:
     json.dump(..., ensure_ascii=False, indent=2)
     """
     pass 
+
+
+json_to_csv(r"C:\Users\First\Documents\GitHub\python_labs\src\data\lab_05\people.json", '')
