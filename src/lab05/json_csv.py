c467ff31ee60882ebb6a8fc816c7b2d164b661cc
pakhomovanastya <pakhomova.nastya.lol@gmail.com>
2025-11-10 19:49:21 +0300

    update function csv_to_xlsx (fix column width)

--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -1,45 +1,47 @@
-import json
 import csv
+import json
+from pprint import pprint
+from pathlib import Path
+
 def json_to_csv(json_path: str, csv_path: str) -> None:
     """
     Преобразует JSON-файл в CSV.
     Поддерживает список словарей [{...}, {...}], заполняет отсутствующие поля пустыми строками.
     Кодировка UTF-8. Порядок колонок — как в первом объекте или алфавитный (указать в README).
     """
-    try:
-        with open(json_path , encoding="utf-8") as f:
-            people = json.load(f)
-    except json.decoder.JSONDecodeError:
-        raise ValueError("Пустой JSON или неподдерживаемая структура")
-    for p in people:
-        if type(p)!=dict:
-            raise ValueError("Список с не-словарами")
-    with open(csv_path,'w', newline="",encoding="utf-8" ) as f:
-        # writer_csv = csv.DictWriter(f, ["name", "age", "city"])
-        writer_csv = csv.DictWriter(f, people[0].keys())
-        writer_csv.writeheader()
-        writer_csv.writerows(people)
-        
-    
+    json_path = Path(json_path) #преобразуем в объекты типа Path, чтобы мы работали с путями
+    csv_path = Path(csv_path) 
+    if json_path.suffix.casefold() != ".json": #расширение файла у нашего относительного пути
+        raise ValueError('Неверный тип файла для аргумента json_path')
+    if csv_path.suffix.casefold() != ".csv":
+        raise ValueError('Неверный тип файла для аргумента csv_path')
+    with open (json_path, "r", encoding='utf-8') as fr: #r для чтения файла
+        data = json.load(fr) #список словарей, load метод загрузки из библиотеки json
+        with open (csv_path, "w", encoding='utf-8', newline="") as fw:
+            writer = csv.DictWriter(fw, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
+
+
 def csv_to_json(csv_path: str, json_path: str) -> None:
     """
     Преобразует CSV в JSON (список словарей).
     Заголовок обязателен, значения сохраняются как строки.
     json.dump(..., ensure_ascii=False, indent=2)
     """
-    try:
-        with open(csv_path , encoding="utf-8") as f:
-            people = csv.DictReader(f)
-            rows = list(people)
-    except :
-        raise FileNotFoundError("Осутствующий файл")
-    if not rows:
-        raise ValueError("Пустой CSV")
-    if not people.fieldnames:
-        raise ValueError("CSV без заголовка")
-    with open(json_path,'w', encoding="utf-8" ) as f:
-        json.dump(rows, f, ensure_ascii=False, indent=2)
+    json_path = Path(json_path) #преобразуем в объекты типа Path, чтобы мы работали с путями
+    csv_path = Path(csv_path) 
+    if json_path.suffix.casefold() != ".json": #расширение файла у нашего относительного пути
+        raise ValueError('Неверный тип файла для аргумента json_path')
+    if csv_path.suffix.casefold() != ".csv":
+        raise ValueError('Неверный тип файла для аргумента csv_path')
+    
+    with open (csv_path, "r", encoding='utf-8') as fr: #r для чтения файла
+        reader = csv.DictReader(fr) #объект
+        data = list(reader) #не в виде объекта а преобразовать в список словарей для стандартной работы с данными
+        
+        with open (json_path, "w", encoding='utf-8') as fw:
+            json.dump(data, fw, ensure_ascii=False, indent=2) #записать в файл;
 
-
-# json_to_csv(r"GitHub\python_labs\src\data\lab_05\people.json", r"GitHub\python_labs\src\data\lab_05\people_from_json.csv")
-# csv_to_json(r"GitHub\python_labs\src\data\lab_05\people.csv", r"GitHub\python_labs\src\data\lab_05\people_from_csv.json")
+json_to_csv("data/lab_05/people.json", "data/lab_05/people_from_json.csv") #относительной пусть онтосительно этого файла
+csv_to_json("data/lab_05/people.csv", "data/lab_05/people_from_csv.json")
